2c9f56e5666f767798e8b89242bc98cd156112ec
btsaubt <btsaubt>
2017-12-13 20:59:42 -0500

    added sast (not implemented yet)

---
+++ sast.ml
@@ -0,0 +1,43 @@
+(* Semantically Checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+(* sexpressions, ssome swith sdatatype sas sadditional sinformation *)
+type sexpr =
+    SInt_Literal of int
+  | SFloat_Literal of float
+  | SString_Literal of string
+  | SBool_Literal of bool
+  | SId of string
+  | SBinop of sexpr * op * sexpr * typ
+  | SUnop of uop * sexpr * typ
+  | SAssign of string * sexpr * typ
+(*  | SVecAccess of string * expr
+  | SMatAccess of string * expr * expr *)
+  | SCall of string * sexpr list * typ
+  | SNoexpr
+
+(* sstatements *)
+type sstmt = 
+    SBlock of sstmt list
+  | SExpr of sexpr
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sexpr * sstmt
+  | SWhile of sexpr * sstmt
+  | SBreak
+  | SContinue 
+
+(* type svdecl = *)
+
+(* sfunction sdeclarations *)
+type sfunc_decl = {
+  styp : typ;
+  sfname : string;
+  sformals : bind list;
+  slocals : bind list;
+  sbody : sstmt list;
+}
+
+(* sprogram *)
+type sprogram = bind list * func_decl list
